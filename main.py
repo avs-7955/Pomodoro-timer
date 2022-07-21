34ba4e06740c3c0291b4c84096cbccb13ef6a813
Ayushi Vineet Sharan <ayushi.v.sharan@gmail.com>
2022-07-21 23:59:20 +0530

    Basic_count_down + Start_button_command

--- main.py
+++ main.py
@@ -14,7 +14,20 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 
+
+def start_timer():
+    '''To start the timer as soon as the user clicks start'''
+    count_down_timer(5)
+
 # ---------------------------- COUNTDOWN MECHANISM ----------------------------- #
+# window.after causes delay and it's functionality is similar to sleep function.
+
+
+def count_down_timer(count):
+    canvas.itemconfig(timer_count_text, text=count)
+    if count > 0:
+        window.after(1000, count_down_timer, count-1)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -27,16 +40,15 @@ window.config(padx=100, pady=50, bg=YELLOW)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text="00:00", fill="white",
-                   font=(FONT_NAME, 25, "bold"))
+timer_count_text = canvas.create_text(100, 130, text="00:00", fill="white",
+                                      font=(FONT_NAME, 25, "bold"))
 canvas.grid(column=1, row=1)
-
 
 title_label = Label(text="Timer", font=(FONT_NAME, 40, "bold"))
 title_label.grid(column=1, row=0)
 title_label.config(fg=GREEN, bg=YELLOW)
 
-start_button = Button(text="Start", highlightthickness=0)
+start_button = Button(text="Start", highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
 
 
