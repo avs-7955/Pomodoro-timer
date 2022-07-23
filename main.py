f6b5334d49c97e820e948f86001eafbbb555bc64
Ayushi Vineet Sharan <ayushi.v.sharan@gmail.com>
2022-07-23 13:00:04 +0530

    Reps add

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 1
 
 # ---------------------------- TIMER RESET ------------------------------- #
 
@@ -18,7 +19,16 @@ LONG_BREAK_MIN = 20
 
 def start_timer():
     '''To start the timer as soon as the user clicks start'''
-    count_down_timer(15)
+    # Time passes as parameter is in seconds.
+    global reps
+    if reps % 8 == 0:
+        count_down_timer(LONG_BREAK_MIN * 60)
+    elif reps % 2 == 0:
+        count_down_timer(SHORT_BREAK_MIN * 60)
+    else:
+        count_down_timer(WORK_MIN * 60)
+    reps += 1
+
 
 # ---------------------------- COUNTDOWN MECHANISM ----------------------------- #
 # window.after causes delay and it's functionality is similar to sleep function.
@@ -37,6 +47,8 @@ def count_down_timer(count):
     canvas.itemconfig(timer_count_text, text=time_left)
     if count > 0:
         window.after(1000, count_down_timer, count-1)
+    else:
+        start_timer()
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -54,17 +66,20 @@ timer_count_text = canvas.create_text(100, 130, text="00:00", fill="white",
                                       font=(FONT_NAME, 25, "bold"))
 canvas.grid(column=1, row=1)
 
+# Label for the title
 title_label = Label(text="Timer", font=(FONT_NAME, 40, "bold"))
 title_label.grid(column=1, row=0)
 title_label.config(fg=GREEN, bg=YELLOW)
 
+# Start button
 start_button = Button(text="Start", highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
 
-
+# Reset button
 reset_button = Button(text="Reset", highlightthickness=0)
 reset_button.grid(column=2, row=2)
 
+# Tick labels for each pomodoro session completion
 tick_labels = Label(text="✔", font=(FONT_NAME, 20,))
 tick_labels.grid(column=1, row=3)
 tick_labels.config(fg=GREEN, bg=YELLOW)
